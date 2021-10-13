149e69616d2430388dbac5e4922a620bf0e613fd
loud-coypu <loud.coypu-dev@yahoo.com>
2021-10-13 18:35:51 +0200

    fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 import Katanda from './Katanda';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Katanda></Katanda>
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      countries: [], stats: []
+    }
+  }
+  async componentDidMount(){
+    const resp = await fetch('https://api.covid19api.com/countries');
+    const countries = await resp.json();
+
+    this.setState({countries: countries});
+    const stats = countries.map(c => 66);
+    this.setState({stats: stats});
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>Countries</h1>
+        {
+          this.state.stats.map(country => <h2>{country}</h2>)
+        }
+        
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Katanda.js
+++ src/Katanda.js
@@ -3,15 +3,20 @@ import React from "react";
 class Katanda extends React.Component{
     constructor(){
         super();
+        this.katuni = React.createRef();
         this.state = {
             name:"Francesco"
         }
     }
     render(){
         return(
-            <div>
+            <div ref={this.katuni}>
+                <p>
                 Wakanda foreva, {this.state.name}!!!
+                </p>
+                <button onClick={() => this.setState({name: 'Michael'})}>Change name</button>
             </div>
+            
         )
     }
 }
